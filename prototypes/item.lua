7543e2ab3b1961be63dafe065b9ecaf21ceb39dd
The-Masca <cristian.masca07@gmail.com>
2026-02-14 19:08:48 +0100

    Add files via upload

---
+++ prototypes/item.lua
@@ -0,0 +1,15 @@
+
+data:extend({
+  {
+    type = "tool",
+    name = "nauvis-science-pack",
+    localised_description = {"item-description.science-pack"},
+    icon = "__base__/graphics/icons/space-science-pack.png",
+    icon_size = 64,
+    subgroup = "science-pack",
+    order = "g",
+    stack_size = 200,
+    weight = 1*kg,
+    durability = 1
+  }
+})
